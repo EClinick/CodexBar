895de6baa27777a7a3621f360185eca8853dc88f
Ethan Clinick <Ethan@Clinick.net>
2026-07-28 05:05:42 -0700

    Split chart attribution sort coverage

--- Sources/CodexBar/CostHistoryChartMenuView.swift
+++ Sources/CodexBar/CostHistoryChartMenuView.swift
@@ -806,32 +806,6 @@ struct CostHistoryChartMenuView: View {
             }
     }
 
-    static func orderedBreakdownItems(
-        _ breakdown: [CostUsageDailyReport.ModelBreakdown]) -> [CostUsageDailyReport.ModelBreakdown]
-    {
-        breakdown.sorted { lhs, rhs in
-            let lCost = lhs.costUSD ?? -1
-            let rCost = rhs.costUSD ?? -1
-            if lCost != rCost {
-                return lCost > rCost
-            }
-
-            let lTokens = lhs.totalTokens ?? -1
-            let rTokens = rhs.totalTokens ?? -1
-            if lTokens != rTokens {
-                return lTokens > rTokens
-            }
-
-            if lhs.modelName != rhs.modelName {
-                return lhs.modelName > rhs.modelName
-            }
-
-            let lhsAttribution = lhs.attribution?.deterministicSortKey ?? ""
-            let rhsAttribution = rhs.attribution?.deterministicSortKey ?? ""
-            return lhsAttribution > rhsAttribution
-        }
-    }
-
     static func detailViewportRowCount(itemCount: Int) -> Int {
         min(max(itemCount, 0), self.maxVisibleDetailLines)
     }
@@ -904,6 +878,32 @@ struct CostHistoryChartMenuView: View {
 }
 
 extension CostHistoryChartMenuView {
+    static func orderedBreakdownItems(
+        _ breakdown: [CostUsageDailyReport.ModelBreakdown]) -> [CostUsageDailyReport.ModelBreakdown]
+    {
+        breakdown.sorted { lhs, rhs in
+            let lCost = lhs.costUSD ?? -1
+            let rCost = rhs.costUSD ?? -1
+            if lCost != rCost {
+                return lCost > rCost
+            }
+
+            let lTokens = lhs.totalTokens ?? -1
+            let rTokens = rhs.totalTokens ?? -1
+            if lTokens != rTokens {
+                return lTokens > rTokens
+            }
+
+            if lhs.modelName != rhs.modelName {
+                return lhs.modelName > rhs.modelName
+            }
+
+            let lhsAttribution = lhs.attribution?.deterministicSortKey ?? ""
+            let rhsAttribution = rhs.attribution?.deterministicSortKey ?? ""
+            return lhsAttribution > rhsAttribution
+        }
+    }
+
     struct RenderFingerprint: Equatable {
         let currencyCode: String
         let historyDays: Int
